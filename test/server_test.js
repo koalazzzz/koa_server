e66664bed9011077be22db85ba79689f4b740f99
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-29 10:36:56 -0700

    updated server

--- test/server_test.js
+++ test/server_test.js
@@ -11,7 +11,7 @@ const request = chai.request;
 process.env.MONGOLAB_URI = 'mongodb://localhost/jazzDb_test';
 const server = require(__dirname + '/../lib/server');
 
-describe('Jazzmaster API', () => {
+describe('Jazzmaster API', () => {c
   after((done) => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.disconnect();
